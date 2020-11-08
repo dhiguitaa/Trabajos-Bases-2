9e44d6e02a984ca89aef398c194d033f0be66b3b
Augusto Balbin Restrepo <50757121+abalbinr@users.noreply.github.com>
2020-11-08 18:31:08 -0500

    Funciona todo

--- Trabajo 4/parte1p3.java
+++ Trabajo 4/parte1p3.java
@@ -27,6 +27,8 @@ public class parte1p3 {
   List<Double> listaMiner1;
   List<Double> listaMiner2;
   double ValorUsdMayor;
+  String miner1;
+  String miner2;
 
   public class ventana2 implements ActionListener {
 
@@ -79,7 +81,7 @@ public class parte1p3 {
       }
 
       try { // Se establece la conexi�n con la base de datos Oracle Express
-        conn = DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-LV4ONBF:1521:xe", "dani", "dani");
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-KBMHID4:1521:xe","augusto","rxnlomejor");
         sentencia = conn.createStatement();
       } catch (final SQLException err) {
         System.out.println("No hay conexi�n con la base de datos.");
@@ -87,6 +89,7 @@ public class parte1p3 {
       }
 
       List<List<String>> bloques = new ArrayList<List<String>>();
+      List<String> codigosMiner = new ArrayList<String>();
       try {
         resultado = sentencia.executeQuery(
             "select id, miner, time_bloq, value_usd from bloque,transaccion where bloque.id = transaccion.block_id");
@@ -113,117 +116,155 @@ public class parte1p3 {
 
           index++;
 
+          codigosMiner.add(resultado.getString("miner"));
         }
 
       } catch (Exception err) {
         System.out.println("catch: " + err);
       }
 
-      int horaInicio = Integer.parseInt(JThoraInicio.getText().split(":")[0]);
-      int minutoInicio = Integer.parseInt(JThoraInicio.getText().split(":")[1]);
-      int horaFinal = Integer.parseInt(JThoraFin.getText().split(":")[0]);
-      int minutoFinal = Integer.parseInt(JThoraFin.getText().split(":")[1]);
-
-      // Se pasan a minutos todo para que sea mas facil la comparación
-      int minutosIniciales = horaInicio * 60 + minutoInicio;
-      int minutosFinales = horaFinal * 60 + minutoFinal;
-
+      
       // Miners
-      String miner1 = JTminer1.getText();
-      String miner2 = JTminer2.getText();
-      listaMiner1 = new ArrayList<Double>(); // Contiene valor Usd de los bloques
-      listaMiner2 = new ArrayList<Double>(); // Contiene valor Usd de los bloques
+      miner1 = JTminer1.getText();
+      miner2 = JTminer2.getText();
 
-      // ValorUsdMayor guardará el valor mas grande para saber que tan grande hacer la
-      // grafica
-      ValorUsdMayor = 0;
+      if (codigosMiner.contains(miner1) && codigosMiner.contains(miner2)){
 
-      for (int i = 0; i < bloques.size(); i++) {
-        int horaBloque = Integer.parseInt(bloques.get(i).get(2).split(" ")[1].split(":")[0]);
-        int minutosBloque = Integer.parseInt(bloques.get(i).get(2).split(" ")[1].split(":")[1]);
-        int minutosTotales = horaBloque * 60 + minutosBloque;
+        int horaInicio = Integer.parseInt(JThoraInicio.getText().split(":")[0]);
+        int minutoInicio = Integer.parseInt(JThoraInicio.getText().split(":")[1]);
+        int horaFinal = Integer.parseInt(JThoraFin.getText().split(":")[0]);
+        int minutoFinal = Integer.parseInt(JThoraFin.getText().split(":")[1]);
 
-        if (minutosTotales >= minutosIniciales && minutosTotales <= minutosFinales) {
-          // Está en el intervalo de tiempo
-          double valueUsdBloque = Double.parseDouble(bloques.get(i).get(3));
-          if (valueUsdBloque > 0) {
-            // Cumple condición de value Usd
-            String minerBloque = bloques.get(i).get(1);
-            if (minerBloque.equals(miner1)) {
-              listaMiner1.add(valueUsdBloque);
-              if (valueUsdBloque > ValorUsdMayor) {
-                ValorUsdMayor = valueUsdBloque;
-              }
-            } else if (minerBloque.equals(miner2)) {
-              listaMiner2.add(valueUsdBloque);
-              if (valueUsdBloque > ValorUsdMayor) {
-                ValorUsdMayor = valueUsdBloque;
+        // Se pasan a minutos todo para que sea mas facil la comparación
+        int minutosIniciales = horaInicio * 60 + minutoInicio;
+        int minutosFinales = horaFinal * 60 + minutoFinal;
+
+        //lista de miners
+        listaMiner1 = new ArrayList<Double>(); // Contiene valor Usd de los bloques
+        listaMiner2 = new ArrayList<Double>(); // Contiene valor Usd de los bloques
+
+        // ValorUsdMayor guardará el valor mas grande para saber que tan grande hacer la
+        // grafica
+        ValorUsdMayor = 0;
+
+        for (int i = 0; i < bloques.size(); i++) {
+          int horaBloque = Integer.parseInt(bloques.get(i).get(2).split(" ")[1].split(":")[0]);
+          int minutosBloque = Integer.parseInt(bloques.get(i).get(2).split(" ")[1].split(":")[1]);
+          int minutosTotales = horaBloque * 60 + minutosBloque;
+
+          if (minutosTotales >= minutosIniciales && minutosTotales <= minutosFinales) {
+            // Está en el intervalo de tiempo
+            double valueUsdBloque = Double.parseDouble(bloques.get(i).get(3));
+            if (valueUsdBloque > 0) {
+              // Cumple condición de value Usd
+              String minerBloque = bloques.get(i).get(1);
+              if (minerBloque.equals(miner1)) {
+                listaMiner1.add(valueUsdBloque);
+                if (valueUsdBloque > ValorUsdMayor) {
+                  ValorUsdMayor = valueUsdBloque;
+                }
+              } else if (minerBloque.equals(miner2)) {
+                listaMiner2.add(valueUsdBloque);
+                if (valueUsdBloque > ValorUsdMayor) {
+                  ValorUsdMayor = valueUsdBloque;
+                }
               }
             }
           }
         }
+
+        // llama la función que dibuja
+        dibujo2 DrawWindow = new dibujo2();
+        DrawWindow.setSize(700, 700);
+        DrawWindow.setResizable(false);
+        DrawWindow.setTitle("Pintando locales y ventas");
+        DrawWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DrawWindow.setVisible(true);
+
+      }else{
+        JOptionPane.showMessageDialog(null, "Alguno de los 2 miner no existe en la base de datos, entonces no se grafica.");
       }
-
-      System.out.println("Puntos de Miner 1");
-      for (int i = 0; i < listaMiner1.size(); i++) {
-        System.out.println(listaMiner1.get(i));
-      }
-
-      System.out.println("Puntos de Miner 2");
-      for (int i = 0; i < listaMiner2.size(); i++) {
-        System.out.println(listaMiner2.get(i));
-      }
-
-      // llama la función que dibuja
-      dibujo2 DrawWindow = new dibujo2();
-      DrawWindow.setSize(960, 960);
-      DrawWindow.setResizable(true);
-      DrawWindow.setTitle("Pintando locales y ventas");
-      DrawWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      DrawWindow.setVisible(true);
-
     };
   };
 
   public class dibujo2 extends JFrame {
 
     public void paint(Graphics g) {
-      g.setColor(Color.lightGray);
-      int Valormayor = (int) ValorUsdMayor;
-      g.fillRect(20, 40, 900, 900);
+      g.setColor(Color.WHITE);
+      g.fillRect(0, 0, 700, 700);
+
+      g.setColor(Color.black);
+      g.drawLine(0+40, 550+60, 550+60, 550+60); //Eje x
+      g.setColor(Color.black);
+      g.drawLine(0+40, 550+60, 0+40, 0+60);//Eje y
+
       Collections.sort(listaMiner1);
       Collections.sort(listaMiner2);
-      int min1 = Collections.min(listaMiner1).intValue();
-      int min2 = Collections.min(listaMiner2).intValue();
 
-      if (min1>min2){
-        min1 = min2;
-      }
       int i = 0;
       while (i < listaMiner1.size()) {
         int suma = 1;
         int j = i;
-        if (!(i == listaMiner1.size() - 1)) {
+        if (i != listaMiner1.size() - 1) {
           while (Double.compare(listaMiner1.get(i), listaMiner1.get(j + 1)) == 0) {
             suma++;
             j++;
+            if (j == listaMiner1.size()-1){
+              break;
+            }
           }
         }
-        // int escala = (int) (900/ValorUsdMayor);
-        int valor = listaMiner1.get(i).intValue();
-        int x = valor/1000;
-        int y = valor/1000;
-        y = (Valormayor/1000)-y;
+
+        double escala = 550/ValorUsdMayor;
+        double valor = listaMiner1.get(i);
+        double x = valor*escala;
+        double y = 550-x;
+
         g.setColor(Color.red);
-        g.drawOval(x + 20, y + 40, 4, 4);
+        g.drawOval((int)x + 50, (int)y + 50, 5, 5);
+
         if (suma > 1) {
-          g.drawString("(" + x + "," + suma + ")", x + 20, y + 50);
+          g.drawString("(" + valor + " , " + suma + ")", (int)x + 50, (int)y + 50+15);
         } else {
-          g.drawString(String.valueOf(listaMiner1.get(i)), x + 20, y + 50);
+          g.drawString(String.valueOf(listaMiner1.get(i)), (int)x + 50, (int)y + 50+15);
         }
         i += suma;
       }
 
+      if(miner1 != miner2){
+        i = 0;
+        while (i < listaMiner2.size()) {
+          int suma = 1;
+          int j = i;
+          if (i != listaMiner2.size() - 1) {
+            while (Double.compare(listaMiner2.get(i), listaMiner2.get(j + 1)) == 0) {
+              suma++;
+              j++;
+              if (j == listaMiner2.size()-1){
+                break;
+              }
+            }
+          }
+  
+          double escala = 550/ValorUsdMayor;
+          double valor = listaMiner2.get(i);
+          double x = valor*escala;
+          double y = 550-x;
+  
+          g.setColor(Color.blue);
+          g.drawOval((int)x + 50, (int)y + 50, 5, 5);
+  
+          if (suma > 1) {
+            g.drawString("(" + valor + " , " + suma + ")", (int)x + 50, (int)y + 50+10);
+          } else {
+            g.drawString(String.valueOf(listaMiner2.get(i)), (int)x + 50, (int)y + 50+10);
+          }
+          i += suma;
+        }
+      }else{
+        System.out.println("Miner iguales, entonces solo se grafica 1");
+      }
+      
     }
   }
 

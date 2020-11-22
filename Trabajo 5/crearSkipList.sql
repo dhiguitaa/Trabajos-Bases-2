6e31d48c553d2ef2e8400f28344c174e65602ba5
Daniela <dhiguitaa17@gmail.com>
2020-11-21 19:03:01 -0500

    identifica grupo de punteros

--- Trabajo 5/crearSkipList.sql
+++ Trabajo 5/crearSkipList.sql
@@ -1,14 +1,15 @@
 DECLARE
-  TYPE tipo_int IS TABLE OF NUMBER
-  INDEX BY BINARY_INTEGER;
+  TYPE tipo_int IS TABLE OF NUMBER INDEX BY BINARY_INTEGER;
   codigos tipo_int;   
+  punterosPorNodo tipo_int;   
   cantidadPunteros tipo_int;   
   swapped BOOLEAN;
   tmp     VARCHAR2(10);
   indice NUMBER(30);
   maxPtrs NUMBER(10);
+  aux NUMBER(10);
 BEGIN
-  indice :=0;
+  indice :=1;
   FOR fila IN (SELECT codigoD FROM departamento)
   LOOP
     codigos(indice) := fila.codigoD;
@@ -37,20 +38,35 @@ BEGIN
    EXIT WHEN NOT swapped;
 
   END LOOP; 
-
+  
   -- random
   maxPtrs := 8;
-
-  for i in 1 .. codigos.LAST LOOP
+  cantidadPunteros(1) := maxPtrs;
+  for i in 2 .. codigos.LAST+1 LOOP
     cantidadPunteros(i) := round(DBMS_RANDOM.VALUE(1,maxPtrs));
-  END LOOP
-  ; 
-
-  FOR i in cantidadPunteros.FIRST .. cantidadPunteros.LAST
-  LOOP
-    dbms_output.put_line(cantidadPunteros(i));
-
   END LOOP;
+  cantidadPunteros(codigos.LAST+2) := maxPtrs;
+  
+  -- flechas
+  for i in 1 .. cantidadPunteros.LAST-1 LOOP -- NODOS
+    for j in 1.. cantidadPunteros(i) loop -- bloques
+      for k in i+1.. cantidadPunteros.LAST loop -- nodos posteriores
+        if j <= cantidadPunteros(k) THEN
+          punterosPorNodo(j) := k;
+          EXIT;  
+        end if;
+      end loop;
+    end loop;
+
+  punterosPorNodo.delete();
+  END LOOP;
+
+
+  -- FOR i in cantidadPunteros.FIRST .. cantidadPunteros.LAST
+  -- LOOP
+  --   dbms_output.put_line(cantidadPunteros(i));
+
+  -- END LOOP;
 
   
 
---
+++ Trabajo 5/skipList.sql
@@ -0,0 +1,24 @@
+drop table indexdepskip;
+create table indexdepskip(
+  numnodo number(30) PRIMARY KEY,
+  codigoD NUMBER(8) Not null, 
+  nombreD VARCHAR2(20) NOT NULL,
+  direccionD VARCHAR2(20) NOT NULL,
+  ptrback number(30) not null,
+  puntero1 number(30),
+  puntero2 number(30),
+  puntero3 number(30),
+  puntero4 number(30),
+  puntero5 number(30),
+  puntero6 number(30),
+  puntero7 number(30),
+  puntero8 number(30),
+  puntero9 number(30),
+  puntero10 number(30),
+  puntero11 number(30),
+  puntero12 number(30),
+  puntero13 number(30),
+  puntero14 number(30),
+  puntero15 number(30),
+  puntero16 number(30)
+);

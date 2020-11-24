ac7b3f97556daaa388e87bff7fe48db97d557801
Daniela <dhiguitaa17@gmail.com>
2020-11-23 22:37:52 -0500

    crear arbol

--- Trabajo 5/segmentTree.sql
+++ Trabajo 5/segmentTree.sql
@@ -1,6 +1,4 @@
 create type arry is table of number(30);
-
-
 create or replace function segmentTree(arr arry,ss in number,se in number,si in number) return number as
 valor number;
 mid number; 
@@ -11,12 +9,11 @@ begin
     insert into indexsegtree values(si,ss,se,arr(ss),0,0);
     valor := arr(ss);
     return valor;
-  else
-    -- resto nodos
-    mid :=ceil(ss+(se-ss)/2);
-    valor := greatest(segmentTree(arr,ss,mid ,si*2+1),segmentTree(arr,mid+1,se,si*2+2));
-    insert into indexsegtree values(si,ss,se,valor,si*2+1,si*2+2);
-    return valor;
   end if;
+  -- resto nodos
+  mid :=floor  (ss+(se-ss)/2);
+  valor := greatest(segmentTree(arr,ss,mid,si*2),segmentTree(arr,mid+1,se,si*2+1));
+  insert into indexsegtree values(si,ss,se,valor,si*2,si*2+1);
+  return valor;
 end;
 /
--- Trabajo 5/tabla_segment.sql
+++ Trabajo 5/tabla_segment.sql
@@ -7,3 +7,14 @@ create table indexsegtree(
   izq number(30),
   der number(30)
 );
+drop table tash_sultana;
+CREATE TABLE tash_sultana(
+  consecutivo NUMBER(8) PRIMARY KEY, 
+  valornotion NUMBER(8) NOT NULL
+);
+
+insert into tash_sultana values(0,10);
+insert into tash_sultana values(1,30);
+insert into tash_sultana values(4,10);
+insert into tash_sultana values(3,20);
+insert into tash_sultana values(2,66);
